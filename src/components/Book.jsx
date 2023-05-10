9461486db788bf9d716eea98f4681428fefb2e80
zamcham <cristian@zamcham.com>
2023-05-10 16:49:39 -0400

    Refactor form and book component

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,15 +1,21 @@
 import PropTypes from 'prop-types';
 import IndicatorProgress from './IndicatorProgress';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
 
-function Book({ book, onDelete }) {
+function Book({ book }) {
   const percentage = 33;
+  const dispatch = useDispatch();
+  const handleDelete = () => {
+    dispatch(removeBook(book.item_id));
+  };
 
   return (
     <div className="individual-book">
       <h3>{book.title}</h3>
       <p>{book.author}</p>
       <button type="button">Comments</button>
-      <button type="button" onClick={() => onDelete(book.id)}>Remove</button>
+      <button type="button" onClick={handleDelete}>Remove</button>
       <button type="button">Edit</button>
       <div className="progress">
         <div className="circle" />
@@ -27,7 +33,7 @@ function Book({ book, onDelete }) {
 
 Book.propTypes = {
   book: PropTypes.shape({
-    id: PropTypes.number.isRequired,
+    item_id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
   }).isRequired,
